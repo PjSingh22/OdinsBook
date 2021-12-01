cfffebcd1144479cc14c34eb03f8c16b45e88f32
Prabhjot <pjsingh1359@gmail.com>
2021-11-30 19:43:24 -0800

    removed margin gap from flash message

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'seeding...'
-User.create(email: 'john@john.com', password: 'password', username: 'johnboy', name: 'John')
+User.create(email: 'john@gmail.com', password: 'password', username: 'johnboy', name: 'John')
 User.create(email: 'jeff@jeff.com', password: 'password', username: 'jeffdoe', name: 'Jeff')
 puts 'seeded'
