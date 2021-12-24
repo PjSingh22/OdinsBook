7d75e7dd5f58e31a19847c37f621adc6660c64bd
Prabhjot <pjsingh1359@gmail.com>
2021-12-23 22:07:04 -0800

    add friends page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
   get 'friends/index'
   get 'friends/destroy'
   resources :friends, only: [:index, :create, :destroy]
+  resources :user_posts
   resources :friend_requests do
     member do
       post :accept
@@ -13,9 +15,9 @@ Rails.application.routes.draw do
     resources :comments
     resources :likes
   end
-  devise_for :users
-  resources :user_posts
   match 'users/:id' => 'users#show', :as => :user, :via => :get
   match 'pending_requests' => 'friend_requests#index', :as => :pending_requests, :via => :get
+  # match 'friends' => 'friends#index', :as => :friends, :via => :get
+
   root 'user_posts#index'
 end
